6fd2448f01fc073ff4f11919a30f54b5d58685b6
Afuza <theafuza@gmail.com>
2021-05-05 20:23:01 +0700

    Created initial TimerScreen screen
    Will be used to time a task and save the time to task state

---
+++ src/screens/TimerScreen.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+
+const TimerScreen = ({ route }) => {
+  const { task } = route.params;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>{task}</Text>
+      <View style={styles.timer}>
+        <Text>03:53</Text>
+      </View>
+      <View style={styles.controls}></View>
+      <View style={styles.data}></View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 40,
+  },
+  timer: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  controls: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  data: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default TimerScreen;
