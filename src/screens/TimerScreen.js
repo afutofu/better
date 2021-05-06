d8b6d7616f30266b2a3db9d2e85947c748325b75
Afuza <theafuza@gmail.com>
2021-05-06 15:12:25 +0700

    Improved styling, implemented RoundedButton
    Added RoundedButton for control buttons

--- src/screens/TimerScreen.js
+++ src/screens/TimerScreen.js
@@ -1,16 +1,38 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Platform,
+  StatusBar,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+
+import RoundedButton from "../components/RoundedButton";
 
 const TimerScreen = ({ route }) => {
   const { task } = route.params;
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>{task.name}</Text>
-      <View style={styles.timer}>
-        <Text>03:53</Text>
+      <View style={styles.titleArea}>
+        <Text style={styles.title}>{task.name}</Text>
       </View>
-      <View style={styles.controls}></View>
+      <View style={styles.timer}>
+        <Text style={styles.time}>03:53</Text>
+      </View>
+      <View style={styles.controls}>
+        <RoundedButton size={95} textStyle={styles.buttonTextStyle}>
+          Save
+        </RoundedButton>
+        <RoundedButton size={115} textStyle={styles.buttonTextStyle}>
+          Start
+        </RoundedButton>
+        <RoundedButton size={95} textStyle={styles.buttonTextStyle}>
+          Clear
+        </RoundedButton>
+      </View>
       <View style={styles.data}></View>
     </SafeAreaView>
   );
@@ -19,19 +41,34 @@ const TimerScreen = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     backgroundColor: "#fff",
-    padding: 40,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  titleArea: {
+    flex: 0.1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
   timer: {
     flex: 0.3,
     justifyContent: "center",
     alignItems: "center",
   },
+  time: {
+    fontSize: 120,
+  },
   controls: {
     flex: 0.3,
-    justifyContent: "center",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
     alignItems: "center",
+  },
+  buttonTextStyle: {
+    fontSize: 16,
   },
   data: {
     flex: 0.3,
